2709742685d1dc62a27f4ef1120a14534847a87d
Richard Habeeb <richard.habeeb@gmail.com>
2016-06-01 16:29:06 -0500

    Resolving switch issues.

--- controller.py
+++ controller.py
@@ -8,11 +8,11 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
-from ryu.lib.mac import haddr_to_bin
 from ryu.lib.ip import ipv4_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from ryu.lib.packet import in_proto
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import tcp
 from ryu.lib.packet import udp
@@ -34,33 +34,40 @@ class controller(app_manager.RyuApp):
         if self.datapath is None:
             return
 
-        match = self.datapath.ofproto_parser.OFPMatch(self.datapath.ofproto.OFPFW_ALL, 0, 0, 0,
-                                                      0, 0, 0, 0, 0, 0, 0, 0, 0)
-        stats = self.datapath.ofproto_parser.OFPFlowStatsRequest(self.datapath, 0, match,
-                                                                 0xff, self.datapath.ofproto.OFPP_NONE)
-        self.datapath.send_msg(stats)
+        #match = self.datapath.ofproto_parser.OFPMatch(self.datapath.ofproto.OFPFW_ALL, 0, 0, 0,
+        #                                              0, 0, 0, 0, 0, 0, 0, 0, 0)
+        #stats = self.datapath.ofproto_parser.OFPFlowStatsRequest(self.datapath, 0, match,
+        #                                                         0xff, self.datapath.ofproto.OFPP_NONE)
+        #self.datapath.send_msg(stats)
 
-    def add_flow(self, in_port, nw_src, tp_src, nw_dst, tp_dst, actions):
-        ofproto = self.datapath.ofproto
+    def add_flow(self, in_port, nw_src, tp_src, nw_dst, tp_dst, nw_proto, actions):
+        ofp = self.datapath.ofproto
 
         match = self.datapath.ofproto_parser.OFPMatch(
-            in_port=in_port, nw_src=nw_src, tp_src=tp_src, nw_dst=nw_dst, tp_dst=tp_dst)
+            dl_type=ether_types.ETH_TYPE_IP,
+            nw_proto=nw_proto,
+            in_port=in_port,
+            nw_src=nw_src,
+            tp_src=int(tp_src),
+            nw_dst=nw_dst,
+            tp_dst=int(tp_dst))
 
-        mod = self.datapath.ofproto_parser.OFPFlowMod(
-            datapath=self.datapath, match=match, cookie=0,
-            command=ofproto.OFPFC_ADD, idle_timeout=10, hard_timeout=0,
-            priority=ofproto.OFP_DEFAULT_PRIORITY,
-            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
-        self.datapath.send_msg(mod)
+        self.logger.info("add flow %i %i:%i -> %i:%i Wildcard:%s", match.in_port, match.nw_src, match.tp_src, match.nw_dst, match.tp_dst, hex(match.wildcards))
+
+        self.datapath.send_msg(self.datapath.ofproto_parser.OFPFlowMod(
+            datapath=self.datapath,
+            match=match,
+            cookie=0,
+            command=ofp.OFPFC_ADD,
+            idle_timeout=10,
+            hard_timeout=800,
+            priority=ofp.OFP_DEFAULT_PRIORITY,
+            flags=ofp.OFPFF_SEND_FLOW_REM,
+            actions=actions))
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
-        if self.datapath is None:
-            self.datapath = msg.datapath
-
-        ofproto = self.datapath.ofproto
-
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
 
@@ -68,35 +75,42 @@ class controller(app_manager.RyuApp):
             # ignore lldp packet
             return
 
+        if self.datapath is None:
+            self.datapath = msg.datapath
+
+        if self.datapath != msg.datapath:
+            elf.logger.info("Different datapath.")
+
+        ofp = self.datapath.ofproto
         ipv4_layer = pkt.get_protocol(ipv4.ipv4)
         transport_layer = pkt.get_protocol(udp.udp)
+        nw_proto = in_proto.IPPROTO_UDP
 
         if not transport_layer:
             transport_layer = pkt.get_protocol(tcp.tcp)
+            nw_proto = in_proto.IPPROTO_TCP
 
+        out_port = ofp.OFPP_FLOOD
         if msg.in_port != controller.SECURITY_DEVICE_SWITCH_PORT:
-            #this could be a security concern. -RTH 6/1
+            # this could be a security concern. -RTH 6/1
             self.mac_to_port[eth.src] = msg.in_port
             out_port = controller.SECURITY_DEVICE_SWITCH_PORT
-            self.logger.info("Forwarding to DPI.")
         elif eth.dst in self.mac_to_port:
             out_port = self.mac_to_port[eth.dst]
-            self.logger.info("Received from DPI.")
-        else:
-            out_port = ofproto.OFPP_FLOOD
-            self.logger.info("Received from DPI, flooding.")
+
+        if out_port == msg.in_port:
+            self.logger.info("Dropping reflected packet.")
+            return
 
         actions = [self.datapath.ofproto_parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD and ipv4_layer and transport_layer:
-            self.logger.info("add flow %s %s:%i -> %s:%i", msg.in_port, ipv4_layer.src,
-                             transport_layer.src_port, ipv4_layer.dst, transport_layer.dst_port)
+        if out_port != ofp.OFPP_FLOOD and ipv4_layer and transport_layer:
             self.add_flow(msg.in_port, ipv4_layer.src, transport_layer.src_port,
-                          ipv4_layer.dst, transport_layer.dst_port, actions)
+                          ipv4_layer.dst, transport_layer.dst_port, nw_proto, actions)
 
         data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
             data = msg.data
 
         out = self.datapath.ofproto_parser.OFPPacketOut(
@@ -108,3 +122,20 @@ class controller(app_manager.RyuApp):
     def _flow_stats_handler(self, ev):
         msg = ev.msg
         self.logger.info("Flow stats reply")
+
+    @set_ev_cls(ofp_event.EventOFPErrorMsg, MAIN_DISPATCHER)
+    def _error_handler(self, ev):
+        msg = ev.msg
+        ofp = self.datapath.ofproto
+        self.logger.info(
+            "EventOFPErrorMsg received.\n"
+            "version=%s, msg_type=%s, msg_len=%s, xid=%s\n"
+            " `-- msg_type: %s\n"
+            "OFPErrorMsg(type=%s, code=%s)\n"
+            " |-- type: %s\n"
+            " |-- code: %s",
+            hex(msg.version), hex(msg.msg_type), hex(msg.msg_len),
+            hex(msg.xid), ofp.ofp_msg_type_to_str(msg.msg_type),
+            hex(msg.type), hex(msg.code),
+            ofp.ofp_error_type_to_str(msg.type),
+            ofp.ofp_error_code_to_str(msg.type, msg.code))
